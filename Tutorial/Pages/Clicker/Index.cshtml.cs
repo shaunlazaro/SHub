d88b0d8fa3afefad933b2d2def08d2741b24e0c0
shaunlazaro <shaunlazaro@gmail.com>
2021-12-07 23:07:21 -0500

    Clicker layout

--- Tutorial/Pages/Clicker/Index.cshtml.cs
+++ Tutorial/Pages/Clicker/Index.cshtml.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Tutorial.Managers;
 using ElectronNET.API;
+using Newtonsoft.Json;
 
 namespace Tutorial.Pages.Clicker
 {
     public class IndexModel : PageModel
     {   
+        
+
+
         public void OnGet()
         {
             InitEvents();
